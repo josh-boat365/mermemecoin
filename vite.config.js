1f804cd9a91dca4019a376239a5128d311cfa22c
josh-boat365 <nyarko.boateng20@outlook.com>
2025-05-16 12:40:56 +0000

    configured vite for tailwindcss v4.1

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
