9b85ed63e47e40bc2c41c31b15eb89ec5d58150c
josh-boat365 <nyarko.boateng20@outlook.com>
2025-05-21 08:19:06 +0000

    Newsletter and CTA section

---
+++ src/components/CTA.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-white right-bg-gradient py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl h-45">
+      <h1 class="text-center uppercase text-4xl font-bold">join the mermeme community</h1>
+      <p class="text-center capitalize font-semibold text-green-600 text-sm">stay updated with the latest news and updates from the world of mermeme
+        coin</p>
+      <div class="flex gap-3 mx-auto justify-center mt-6">
+        <input type="text" placeholder="Enter your email address"
+          class="border bg-gray-50 border-green-500 rounded px-4 py-2 w-lg" />
+        <button type="button"
+          class="inline-flex items-center justify-center h-12 px-6 rounded-md text-white text-lg bg-gradient-to-tr from-[#5affa4] to-[#34af57] shadow-[0_2px_4px_rgba(45,35,66,0.4),0_7px_13px_-3px_rgba(45,35,66,0.3),0_-3px_0_inset_rgba(58,65,111,0.5)] transition-all duration-150 ease-in-out hover:shadow-[0_4px_8px_rgba(45,35,66,0.4),0_7px_13px_-3px_rgba(45,35,66,0.3),0_-3px_0_inset_#3fe07b] hover:-translate-y-0.5 active:shadow-[inset_0_3px_7px_#3fe07b] active:translate-y-0.5 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-[#3fe07b]">
+          Subscribe Now
+      </button>
+      </div>
+      <p class="text-xs capitalize text-center transition py-6">by clicking "subscribe now" you agree to our <a href="#" class="text-indigo-500 hover:text-green-500">terms and conditions</a></p>
+    </div>
+  </div>
+</template>
